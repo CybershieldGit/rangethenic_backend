11936f7e92dff78773c0d9fb919dbdb8e9510833
CST <team.cybershield@gmail.com>
2026-07-08 16:49:30 +0530

    admin pannel

--- models/Category.js
+++ models/Category.js
@@ -22,6 +22,6 @@ const categorySchema = new mongoose.Schema(
   }
 );
 
-const Category = mongoose.model('Category', categorySchema);
+const Category = mongoose.models.Category || mongoose.model('Category', categorySchema);
 
 export default Category;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // We can add config options here if needed later
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
